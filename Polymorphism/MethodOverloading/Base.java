7cfc1f36d8a9e5e3abd54f3abd22081c6b88724d
Noor Mohammed B <nooru.mohad@gmail.com>
2020-04-19 12:54:40 +0530

    MethodOverloading practice

---
+++ Polymorphism/MethodOverloading/Base.java
@@ -0,0 +1,12 @@
+package MethodOverloading;
+
+public class Base {
+    void display()
+    {
+        System.out.println("i am base class display and no argument method");
+    }
+    void display(int a)
+    {
+        System.out.println("i am base class and display with 1 argument method   a : "+a);
+    }
+}
---
+++ Polymorphism/MethodOverloading/app.java
@@ -0,0 +1,13 @@
+package MethodOverloading;
+
+public class app 
+{
+
+    public static void main(String[] args)
+    {
+        Base obj =new Base();
+        obj.display();
+        obj.display(33);        
+    }
+
+}
