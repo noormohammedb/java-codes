4bd32e27ada6ee582e0b2cb8fb3739f90f20388b
Noor Mohammed B <nooru.mohad@gmail.com>
2020-04-19 12:47:36 +0530

    Polymorphism practice

---
+++ Polymorphism/ConstructionOverloading/App.java
@@ -0,0 +1,11 @@
+package ConstructionOverloading;
+public class App
+{
+    public static void main(String cmd[])
+    {
+        // System.out.println("Hello World");
+        // Base obj = new Base();
+        new Base();
+        new Base(3);
+    }
+}
---
+++ Polymorphism/ConstructionOverloading/Base.java
@@ -0,0 +1,14 @@
+package ConstructionOverloading;
+
+public class Base 
+{
+    Base()
+    {
+        System.out.println("i am base constructure and no argument");
+    }
+    Base(int a)
+    {
+        System.out.println("i am base constructure and 1 argument a : "+a);
+    }
+
+}
